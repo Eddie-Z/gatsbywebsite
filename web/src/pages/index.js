bcf866b06f1ddbf4a66719054737dfffc6c63a73
Eddie-Z <35039865+Eddie-Z@users.noreply.github.com>
2021-02-20 18:01:54 -0800

    refactor index.js

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import { mapEdgesToNodes, filterOutDocsWithoutSlugs } from '../lib/helpers'
-import BlogPostPreviewGrid from '../components/blog-post-preview-grid'
 import Container from '../components/container'
 import GraphQLErrorList from '../components/graphql-error-list'
 import ProjectPreviewGrid from '../components/project-preview-grid'
