266ac575a15679a10a1aab17bff0a579cfe445da
Eddie-Z <35039865+Eddie-Z@users.noreply.github.com>
2021-02-20 18:22:06 -0800

    layout refactor

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -8,34 +8,6 @@ const Layout = ({ children, companyInfo, onHideNav, onShowNav, showNav, siteTitl
   <>
     <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} />
     <div className={styles.content}>{children}</div>
-    {/* <footer className={styles.footer}>
-      <div className={styles.footerWrapper}>
-        <div className={styles.companyAddress}>
-          {companyInfo && (
-            <div>
-              {companyInfo.name}
-              <br />
-              {companyInfo.address1}
-              <br />
-              {companyInfo.address2 && (
-                <span>
-                  {companyInfo.address2}
-                  <br />
-                </span>
-              )}
-              {companyInfo.zipCode} {companyInfo.city}
-              {companyInfo.country && <span>, {companyInfo.country}</span>}
-            </div>
-          )}
-        </div>
-
-        <div className={styles.siteInfo}>
-          © {new Date().getFullYear()}, Built with <a href='https://www.sanity.io'>Sanity</a> &amp;
-          {` `}
-          <a href='https://www.gatsbyjs.org'>Gatsby</a>
-        </div>
-      </div>
-    </footer> */}
   </>
 )
 
